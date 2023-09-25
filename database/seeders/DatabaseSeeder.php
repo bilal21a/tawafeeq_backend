f9aca8a7779ff35b9118e5ccd782d683a52b9aa8
Umer Akram <umerakram.6932@gmail.com>
2023-09-26 00:15:38 +0500

    seed for country

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CountrySeeder::class);
         $this->call(PlansSeeder::class);
     }
 }
