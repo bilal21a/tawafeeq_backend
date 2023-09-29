382fef8e213e0d95cf96a70d9c4ffda1d398ad54
Bilal Mac <b.anwar21.a@gmail.com>
2023-09-29 12:59:58 +0500

    ww

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,7 +23,10 @@ class ProfileController extends Controller
         $profilevistors = ProfileVistors::where('visited_to_id', Auth::id())->with('user')->latest()->get();
         if ($request->chat_id != null) {
             $chat = Chats::find($chat_id);
-            if ((!isset($chat)) || ($chat == null && ($chat->initiator_id == auth()->id() || $chat->partner_id == auth()->id()))) {
+            if (!$chat == null) {
+                return redirect()->route('something_went_wrong');
+            }
+            if ($chat->initiator_id != auth()->id() && $chat->partner_id != auth()->id()) {
                 return redirect()->route('something_went_wrong');
             }
             $partner_id = $chat->initiator_id == auth()->id() ? $chat->partner_id : $chat->initiator_id;
