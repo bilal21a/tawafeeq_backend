43a875b062eb7dcd58ad577d97578d34c4fc6eb0
Bilal Mac <b.anwar21.a@gmail.com>
2023-12-10 20:31:37 +0500

    ss

--- resources/views/profile/js/chat_mainjs.blade.php
+++ resources/views/profile/js/chat_mainjs.blade.php
@@ -116,8 +116,7 @@
     function get_chat(chat_id, partner_id) {
         var itemsRef = firebase
         .database()
-        // .ref('messages').orderByChild('chat_id').equalTo(Number.parseInt(chat_id));
-        .ref('messages').orderByChild('chat_id').equalTo(24);
+        .ref('messages').orderByChild('chat_id').equalTo(Number.parseInt(chat_id));
         console.log('itemsRef: ', itemsRef);
 
         itemsRef.on("value", snapshot => {
